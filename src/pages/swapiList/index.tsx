8131dfc1abfe84f315eed77d5399d0784fbd1927
Rodrigo Emmanoel <rodrigoemmanoel98@gmail.com>
2024-05-17 22:52:54 -0300

    FEAT:Populado os componentes com as props, comunicando a lista com os filtros

--- src/pages/swapiList/index.tsx
+++ src/pages/swapiList/index.tsx
@@ -1,13 +1,20 @@
 import styles from "./index.module.css";
 import Filter from "./filter";
 import List from "./list";
+import { useState } from "react";
 
 function SwapiList() {
+  const [search, setSearch] = useState("");
+  const [favorites, setFavorites] = useState(false);
+
   return (
     <div className={styles.swapi}>
       <div className={styles.swapi__card}>
-        <Filter />
-        <List />
+        <Filter
+          onSearchChange={(e) => setSearch(e)}
+          onFavoritesChange={(e) => setFavorites(e)}
+        />
+        <List listSearch={search} onlyFavorites={favorites} />
       </div>
     </div>
   );
