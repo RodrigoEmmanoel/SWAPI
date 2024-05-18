d195c8a61f12b7bad091b337a1c2ca299fbf3171
Rodrigo Emmanoel <rodrigoemmanoel98@gmail.com>
2024-05-17 22:50:23 -0300

    FEAT:
    Componentes para icones em svg

---
+++ src/components/svgImages/svgClose.tsx
@@ -0,0 +1,14 @@
+export default function SvgClose() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32"
+        d="M368 368L144 144M368 144L144 368"
+      />
+    </svg>
+  );
+}
