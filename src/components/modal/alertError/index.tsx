0342c4d8d74cbdd4c9ddd5aff57797da5ff23187
Rodrigo Emmanoel <rodrigoemmanoel98@gmail.com>
2024-05-20 20:09:37 -0300

    FEAT:
    Componente da modal AlertError

---
+++ src/components/modal/alertError/index.tsx
@@ -0,0 +1,34 @@
+// Modal.js
+import style from "./index.module.css";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+}
+
+const Modal = ({ isOpen, onClose, title, message }: ModalProps) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className={style.modal__overlay} onClick={onClose}>
+      <div
+        className={style.modal__content}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className={style.modal__header}>
+          <span className={style.modal__title}>{title}</span>
+        </div>
+        <div className={style.modal__text}>{message}</div>
+        <div className={style.modal__footer}>
+          <button onClick={() => onClose()}>Fechar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
